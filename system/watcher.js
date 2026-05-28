b480b5bb12b06cb05eaae93398100142df16907a
emonbhaiwpbot <emonbhai.wp.bot@gmail.com>
2026-05-29 00:17:32 +0800

    Update watcher.js

--- system/watcher.js
+++ system/watcher.js
@@ -1,12 +1,29 @@
 const chokidar = require("chokidar")
 
+let timeout = null
+
 function watchPlugins(loadPlugins) {
 
-chokidar.watch("./plugins").on("all", async () => {
+const watcher = chokidar.watch(
+"./plugins",
+{
+ignored: /(^|[\\/])\\../,
+persistent: true,
+ignoreInitial: true
+}
+)
+
+watcher.on("change", async() => {
+
+clearTimeout(timeout)
+
+timeout = setTimeout(async() => {
 
 console.log("[ PLUGINS UPDATED ]")
 
 await loadPlugins()
+
+}, 1000)
 
 })
 
